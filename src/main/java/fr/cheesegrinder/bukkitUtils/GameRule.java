a9dd0dc15fed1c5dff0aaf491dfcade56baf6d6e
Sukaato <sukaato.dev@gmail.com>
2021-01-05 10:27:33 +0100

    first commit

---
+++ src/main/java/fr/cheesegrinder/bukkitUtils/GameRule.java
@@ -0,0 +1,30 @@
+package fr.cheesegrinder.bukkitUtils;
+
+public class GameRule {
+    
+    public final static String ANNOUNCE_ADVANCEMENTS         = "announceAdvancements";
+    public final static String COMMAND_BLOCK_OUTPUT          = "commandBlockOutput";
+    public final static String DISABLE_ELYTRA_MOVEMENT_CHECK = "disableElytraMovementCheck";
+    public final static String DO_DAYLIGHT_CYCLE             = "doDaylightCycle";
+    public final static String DO_ENTITY_DROPS               = "doEntityDrops";
+    public final static String DO_FIRE_TICK                  = "doFireTick";
+    public final static String DO_LIMITED_CRAFTING           = "doLimitedCrafting";
+    public final static String DO_MOB_LOOT                   = "doMobLoot";
+    public final static String DO_MOB_SPAWNING               = "doMobSpawning";
+    public final static String DO_TILE_DROPS                 = "doTileDrops";
+    public final static String DO_WEATHER_CYCLE              = "doWeatherCycle";
+    public final static String GAME_LOOP_FUNCTION            = "gameLoopFunction";
+    public final static String KEEP_INVENTORY                = "keepInventory";
+    public final static String LOG_ADMIN_COMMANDS            = "logAdminCommands";
+    public final static String MAX_COMMAND_CHAIN_LENGTH      = "maxCommandChainLength";
+    public final static String MAX_ENTITY_CRAMMING           = "maxEntityCramming";
+    public final static String MOB_GRIEFING                  = "mobGriefing";
+    public final static String NATURAL_REGENERATION          = "naturalRegeneration";
+    public final static String RANDOM_TICK_SPEED             = "randomTickSpeed";
+    public final static String REDUCED_DEBUG_INFO            = "reducedDebugInfo";
+    public final static String SEND_COMMAND_FEEDBACK         = "sendCommandFeedback";
+    public final static String SHOW_DEATH_MESSAGES           = "showDeathMessages";
+    public final static String SPAWN_RADIUS                  = "spawnRadius";
+    public final static String SPECTATOR_GENERATE_CHUNCKS    = "spectatorGenerateChunks";
+
+}
